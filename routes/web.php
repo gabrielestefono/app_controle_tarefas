3a2f0413cba789c627a2aef65d88d66516075560
gabrielestefono <gabrielestefono@hotmail.com>
2023-12-06 08:22:56 -0300

    Forçando verificação de email

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {return view('welcome');});
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('tarefa', TarefaController::class);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+    ->middleware('verified')
+    ->name('home');
+Route::resource('tarefa', TarefaController::class)->middleware('verified');
 
 Route::get('/mensagem-teste', function () {
     return new MensagemTesteMail();
